158ac09fc9752fd28cd74032fcb1d4123e68116c
Dias ZHANDAR <diaszhandar@astri.org>
2022-04-29 03:44:30 +0800

    repo created, initial commit

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Quants-test-from-Itarle
+# Assessment-by-Itarle
+
+We were provided by a dataset (*"scandi.csv"*) consisting from 100 Scandinavian blue chip stocks and their trades over 4 days. The task is to conduct data analysis on the given data and answer the given questions.
+
+Please refer to "*Source.cpp*" file for more details of used functions.
+
+---
+## Pre-processing of dataset
+Before digging into a questions we should make sure that our dataset is consistent and withougt any misalignments.
+
+After reading a csv file and loading it into dataframe via ```readCSV()``` function, we can check the unique stock codes with ```checkStockCodes()``` function. This supposed to return around 100 unique stock codes as it was specified in a task.
+
+After running ```checkStockCodes()```, we confirmed that there are 100 unique stocks as it was supposed.
+![image](./img/checkStocks.JPG)
+
+*(an output of checkStockCodes)*
+
+However, there are a number of specification that we were instructed of.
+1) We should exclude auction trades which are usually conducted twice a day
+    - however, we are including "XT" or empty condition codes.
+    - In detail implementation of this filter could be seen from ```isnotAuction()``` function.
+
+2) We should consider no trading periods as it might introduce large time gaps.
+
+3) I believe that it might be better to sort out the given data in terms of date because this will make sure that two consecutive trades are calculated.
+    - In a trade-off between speed and accuracy, I chose an accuracy as this type of post-analysis might consider accuracy as more important factor.
+
+
+---
+
+## Question 1: mean time between trades
+
+Mean time between trades is calculated by ```meanTimeBetweenTrades()``` function. There are two important features to point out in my calculation of mean time between trades.
+ - Every data row has been sorted by data and time to make sure that there are consecutive trades.
+ - Consecutive trades should be executed in the same date. Otherwise, it is a gap of no trade period.
+
+ Consequently, we recevied the following results:
+
+
+---
+## Question 2: median time between trades
+Median is considered to be a middle element of sorted time differences. Therefore, time differences, which were calculated in similar way as in Question 1, were stored in a vector data structure. Then, a median was found via sorting the derived vector and outputting an element in the **middle** of the vector.
+
+This was calculated by ```medianTimeBetweenTrades()``` function and outputted the following times:
+
+---
+## Question 3: mean time between tick changes
+Tick is a measure of minimum upward or downward movement in the price of a security. In detail, we define two types of tick as a movement indicator - uptick and downtick.
+- Uptick indicates a trade where the transaction has occurred at a price higher than the previous transaction.
+- Downtick indicates a transaction that has occurred at a lower price.
+
+In other words, we can calculate a slope between two consecutive trading points. If the slope is positive, then it is uptick. In opposite case, it is a downtick.
+
+Therefore, we calculate a mean time when tick changes from uptick to downtick or vice versa. For details, please refer to ```meanTimeBetweenTick()``` function.
+
+## #Image with results
+
+---
+
+## Question 4: median time between tick changes
+Procedure would be similar to the previous question. However, we would be saving time differences between tick changes and outputing a middle value after sorting the time differences.
---
+++ Source.cpp
@@ -0,0 +1,354 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+
+
+using namespace std;
+
+
+struct DataRows {
+	string BloombergCode;
+	double bidPrice;
+	double askPrice;
+	double tradePrice;
+	double tradeVol;
+	double date;
+	double secsPastMidnight;
+	string conditionCodes;
+
+	double time;
+};
+
+<<<<<<< HEAD
+
+// helper functions
+bool conditionCodeChecker(const std::string code) {
+	
+	// return True if 
+	if (code.find("XT") != std::string::npos || code.compare("") != 0) {
+		return false;
+	}
+	return true;
+}
+
+=======
+bool compareTime(const DataRows& a, const DataRows& b) {
+	if (a.date != b.date)
+		return a.secsPastMidnight < b.secsPastMidnight;
+	return a.secsPastMidnight < b.secsPastMidnight;
+}
+>>>>>>> 95b73aa533c14dc5f5b1ce0ed599754e854b4d54
+
+
+class ScandiAnalysis {
+public:
+	void readCSV(const std::string& csvFilePath, std::map<std::string, std::vector<DataRows>>& dataframe);
+	bool isnotAuction(const DataRows& data);
+	void checkStockCodes(const std::map<std::string, std::vector<DataRows>>& dataframe);
+	void meanTimeBetweenTrades(const std::map<std::string, std::vector<DataRows>>& dataframe);
+	void medianTimeBetweenTrades(const std::map<std::string, std::vector<DataRows>>& dataframe);
+	void meanTimeBetweenTick(const std::map<std::string, std::vector<DataRows>>& dataframe);
+	void medianTimeBetweenTick(const std::map<std::string, std::vector<DataRows>>& dataframe);
+	void longestTimeBetweenTrades(const std::map<std::string, std::vector<DataRows>>& dataframe);
+	void meanBidAskSpread(const std::map<std::string, std::vector<DataRows>>& dataframe);
+	void medianBidAskSpread(const std::map<std::string, std::vector<DataRows>>& dataframe);
+	vector<DataRows> getDataframe();
+
+private:
+	vector<DataRows> dataframe;
+};
+
+void ScandiAnalysis::readCSV(const std::string& csvFilePath, std::map<std::string, std::vector<DataRows>>& dataframe) {
+
+	fstream fin;
+
+	fin.open(csvFilePath, ios::in);
+
+	vector<string> row;
+	string line, word, temp;
+
+	int count = 0;
+	while (fin >> temp && count < 15000) {
+
+		count++;
+
+		row.clear();
+
+		getline(fin, line);
+
+		stringstream s(temp + line);
+
+		while (getline(s, word, ',')) {
+			row.push_back(word);
+		}
+
+		DataRows temp;
+		temp.BloombergCode = row[0]; temp.bidPrice = stod(row[2]); temp.askPrice = stod(row[3]);
+		temp.tradePrice = stod(row[4]); temp.date = stod(row[10]); temp.secsPastMidnight = stod(row[11]);
+		temp.conditionCodes = row[14]; temp.tradeVol = stod(row[7]);
+
+		// as we should filter out auction trades
+		if (isnotAuction(temp)) {
+			dataframe[row[0]].push_back(temp);
+		}
+	}
+
+	// need to sort each vector afterwards
+	for (auto& stock : dataframe) {
+		sort(stock.second.begin(), stock.second.end(), compareTime);
+	}
+
+}
+bool ScandiAnalysis::isnotAuction(const DataRows& data) {
+
+	if ((data.bidPrice > data.askPrice && (data.conditionCodes != "XT" || data.conditionCodes != "")) || data.bidPrice == 0 || data.askPrice == 0)
+		return false;
+
+	return true;
+}
+
+
+void ScandiAnalysis::checkStockCodes(const std::map<std::string, std::vector<DataRows>>& dataframe) {
+
+	cout << "The number of unique stock codes in the given dataset is " << dataframe.size() << endl;
+
+	cout << "Let's manually look through stock codes:" << endl;
+	for (const auto& stock : dataframe) {
+		cout << stock.first << endl;
+	}
+}
+
+
+void ScandiAnalysis::meanTimeBetweenTrades(const std::map<std::string, std::vector<DataRows>>& dataframe) {
+
+	// exclude no trading periods 
+	// (time between first and last trade - no trading time) / number of trades
+
+
+
+	cout << "---Mean time between trades---" << endl;
+
+
+	for (const auto& stock : dataframe) {
+
+		double sum = 0;
+		int n = stock.second.size(), count = 0;
+
+		for (int i = 0; i < n; i++) {
+
+			// we verify if any trade has happened at that time by checking the volume of trades
+			if (i + 1 < n && stock.second[i].tradeVol > 0 && stock.second[i].date == stock.second[i+1].date) {
+				sum += (stock.second[i + 1].secsPastMidnight - stock.second[i].secsPastMidnight);
+				count += 1;
+			}
+
+		}
+
+		cout << stock.first << ": " << (sum / count) << endl;
+
+	}
+
+}
+
+
+void ScandiAnalysis::medianTimeBetweenTrades(const std::map<std::string, std::vector<DataRows>>& dataframe) {
+
+	cout << "---Median time between trades---" << endl;
+
+	for (const auto& stock : dataframe) {
+
+		vector<double> timeDiff;
+
+		int n = stock.second.size();
+		for (int i = 0; i < n; i++) {
+
+			if (i + 1 < n && stock.second[i].tradeVol > 0 && stock.second[i].date == stock.second[i + 1].date) {
+				timeDiff.push_back(stock.second[i + 1].secsPastMidnight - stock.second[i].secsPastMidnight);
+			}
+
+		}
+
+		sort(timeDiff.begin(), timeDiff.end());
+		if (n % 2 == 1)
+			cout << stock.first << ": " << (timeDiff[n / 2] + timeDiff[n / 2 + 1]) / 2 << endl;
+		else
+			cout << stock.first << ": " << timeDiff[n / 2] << endl;
+	}
+
+}
+
+
+void ScandiAnalysis::longestTimeBetweenTrades(const std::map<std::string, std::vector<DataRows>>& dataframe) {
+
+	cout << "---Longest time between trades---" << endl;
+
+	for (const auto& stock : dataframe) {
+
+		vector<double> timeDiff;
+
+		int n = stock.second.size();
+		for (int i = 0; i < n; i++) {
+
+			if (i + 1 < n && stock.second[i].tradeVol > 0 && stock.second[i].date == stock.second[i + 1].date) {
+				timeDiff.push_back(stock.second[i + 1].secsPastMidnight - stock.second[i].secsPastMidnight);
+			}
+
+		}
+
+		double longest = *max_element(timeDiff.begin(), timeDiff.end());
+		cout << stock.first << ": " << longest << endl;
+	}
+
+}
+
+
+void ScandiAnalysis::meanBidAskSpread(const std::map<std::string, std::vector<DataRows>>& dataframe) {
+
+	cout << "---Mean bid ask spread---" << endl;
+
+	for (const auto& stock : dataframe) {
+
+		double sum = 0;
+		int n = stock.second.size(), count = 0;
+
+		for (int i = 0; i < n; i++) {
+
+			// we verify if any trade has happened at that time by checking the volume of trades
+			if (stock.second[i].askPrice >= stock.second[i].bidPrice) {
+				sum += (stock.second[i].askPrice - stock.second[i].bidPrice);
+				count += 1;
+			}
+
+		}
+
+		cout << stock.first << ": " << (sum / count) << endl;
+
+	}
+
+}
+
+
+void ScandiAnalysis::medianBidAskSpread(const std::map<std::string, std::vector<DataRows>>& dataframe) {
+
+	cout << "---Median bid ask spread---" << endl;
+
+	for (const auto& stock : dataframe) {
+
+		vector<double> bidaskSpread;
+
+		int n = stock.second.size();
+		for (int i = 0; i < n; i++) {
+
+			if (stock.second[i].askPrice >= stock.second[i].bidPrice) {
+				bidaskSpread.push_back(stock.second[i].askPrice - stock.second[i].bidPrice);
+			}
+
+		}
+
+		sort(bidaskSpread.begin(), bidaskSpread.end());
+		if (n % 2 == 1)
+			cout << stock.first << ": " << (bidaskSpread[n / 2] + bidaskSpread[n / 2 + 1]) / 2 << endl;
+		else
+			cout << stock.first << ": " << bidaskSpread[n / 2] << endl;
+	}
+
+}
+
+
+void ScandiAnalysis::meanTimeBetweenTick(const std::map<std::string, std::vector<DataRows>>& dataframe) {
+
+	for (const auto& stock : dataframe) {
+
+		double first_tick = (stock.second[0].tradePrice > stock.second[1].tradePrice) ? 0 : 1;
+		double latestTickChange[2] = { first_tick, stock.second[0].time };
+
+		int count = 1;
+		double sum = stock.second[1].time - stock.second[0].time;
+
+		int n = stock.second.size();
+		for (int i = 1; i < n; i++) {
+
+			if (i + 1 < n) {
+
+				double tick = (stock.second[i].tradePrice > stock.second[i + 1].tradePrice) ? 0 : 1;
+				if (latestTickChange[0] != tick) {
+
+					// tick change happens
+					sum += (stock.second[i].time - latestTickChange[1]);
+					count += 1;
+					latestTickChange[0] = tick; latestTickChange[1] = stock.second[i].time;
+
+				}
+
+			}
+
+		}
+
+		cout << stock.first << ": " << (sum / count) << endl;
+
+	}
+
+}
+
+
+
+void medianTimeBetweenTick(const std::map<std::string, std::vector<DataRows>>& dataframe) {
+
+	for (const auto& stock : dataframe) {
+
+		double first_tick = (stock.second[0].tradePrice > stock.second[1].tradePrice) ? 0 : 1;
+		double latestTickChange[2] = { first_tick, stock.second[0].time };
+
+		std::vector<double> timeDiff;
+
+		int n = stock.second.size();
+		for (int i = 1; i < n; i++) {
+
+			if (i + 1 < n) {
+
+				double tick = (stock.second[i].tradePrice > stock.second[i + 1].tradePrice) ? 0 : 1;
+				if (latestTickChange[0] != tick) {
+
+					// tick change happens
+					timeDiff.push_back(stock.second[i].time - latestTickChange[1]);
+					latestTickChange[0] = tick; latestTickChange[1] = stock.second[i].time;
+
+				}
+
+			}
+
+		}
+
+		sort(timeDiff.begin(), timeDiff.end());
+		if (n % 2 == 1)
+			cout << stock.first << ": " << (timeDiff[n / 2] + timeDiff[n / 2 + 1]) / 2 << endl;
+		else
+			cout << stock.first << ": " << timeDiff[n / 2] << endl;
+
+	}
+
+}
+
+
+int main() {
+
+
+
+	map<string, vector<DataRows>> dataframe;
+	string csvFilePath = "C:\\Users\\diaszhandar\\Downloads\\scandi.csv";
+
+	ScandiAnalysis analysis;
+	analysis.readCSV(csvFilePath, dataframe);
+	analysis.checkStockCodes(dataframe);
+	analysis.meanTimeBetweenTrades(dataframe);
+	analysis.medianTimeBetweenTrades(dataframe);
+	analysis.longestTimeBetweenTrades(dataframe);
+	analysis.meanBidAskSpread(dataframe);
+	analysis.medianBidAskSpread(dataframe);
+
+	return 0;
+}
